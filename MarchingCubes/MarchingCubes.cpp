3d56f4d89a9a3b9105cfd4734b2fe5c46d43d51e
Tom Jackson <tom.jackson1711@gmail.com>
2024-06-18 10:31:01 +0100

    Initial commit
    Initial commit with setup for marching cubes done.

---
+++ MarchingCubes/MarchingCubes.cpp
@@ -0,0 +1,44 @@
+﻿// MarchingCubes.cpp : Defines the entry point for the application.
+//
+
+#include "MarchingCubes.h"
+#include "mc_tables.h"
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	// Read in the scalar field 
+	std::ifstream dField(argv[1]);
+
+	// Dimensions of the scalar field are first in the file
+	long dimension;
+	dField >> dimension;
+	cout << dimension << endl;
+
+	// Define the scalar field container
+	std::vector<std::vector<std::vector<float>>> scalarField(dimension, vector<vector<float>>(dimension, vector<float>(dimension)));
+	int distance = 0;
+	for (int x = 0; x < dimension; x++) {
+		for (int y = 0; y < dimension; y++) {
+			for (int z = 0; z < dimension; z++) {
+				dField >> distance;
+				scalarField[x][y][z] = distance;
+			}
+		}
+	}
+
+	dField.close();
+
+	// March through the distance field
+    cout << edgeTable[2][1] << endl;
+
+	// Get the state of the cube
+
+	// Use lookup table
+
+	// Store resulting triangles
+
+
+	return 0;
+}
---
+++ MarchingCubes/MarchingCubes.h
@@ -0,0 +1,12 @@
+﻿// MarchingCubes.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <string>
